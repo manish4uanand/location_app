f39824e1d50832d2671c47b99c9cc6fd45fcfcfd
manish anand <manish4uanand@gmail.com>
2021-02-16 18:51:39 +0530

    implemented lication controller with index and new

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,26 @@
+class LocationsController < ApplicationController
+
+	def index
+		@locations = Location.order('created_at DESC')
+	end
+
+	def new
+		@location = Location.new
+	end
+
+	def create
+		@location = Location.new(location_params)
+		if @location.save
+		  flash[:success] = "Location added Successfully!"
+		  redirect_to root_path
+		else
+		  render 'new'
+		end
+	end
+
+	private
+
+	def location_params
+		params.require(:location).permit(:address)
+	end
+end
